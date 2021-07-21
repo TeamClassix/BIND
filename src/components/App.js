d1a68c73d7e0612e58e8d0955006d53b94611b68
Jesse <jessemchung1@gmail.com>
2021-07-21 15:58:28 -0700

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Overview from './Overview/Overview.jsx';
 
 const App = () => {
-  return <div>App Component</div>
+  return (
+  <>
+  <div>App Component</div>
+  <Overview />
+  </>
+  )
 }
 
 export default App;
