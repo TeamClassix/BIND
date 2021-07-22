b8c92ac73601acdf184609af99b5af29597d264f
Michael Chan <mhcchan@ucdavis.edu>
2021-07-22 01:43:29 -0700

    Linting Part 1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -10,15 +10,18 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?(js|jsx)$/,
+        test: /\.m?(jsx|js)$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
-  }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
