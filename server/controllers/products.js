84d9e6e3221c380d7191428c81ecba71c6c4b3f9
Michael Chan <mhcchan@ucdavis.edu>
2021-07-24 09:23:02 -0700

    Linting

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 // eslint-disable-next-line import/no-unresolved
-const Models = require('#models');
+// const Models = require('#models');
 
 const router = express.Router({ mergeParams: true });
 
