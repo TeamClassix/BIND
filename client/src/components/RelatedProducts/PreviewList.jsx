97b2c6f3d95282a261689e6594796bc8db316b7d
Michael Chan <mhcchan@ucdavis.edu>
2021-07-25 21:57:56 -0700

    Related Products Init

---
+++ client/src/components/RelatedProducts/PreviewList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PreviewList = ({ title, list }) => {
+  console.log('stop');
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div>
+        {list.map(item => {
+          return <ItemPreview key={item.id} value={item} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default PreviewList;
