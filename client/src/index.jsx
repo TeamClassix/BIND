2d28aab0c1f721cc6eabb18f4cbdb13faa2f686f
Michael Chan <mhcchan@ucdavis.edu>
2021-07-27 00:06:52 -0700

    Loading Screen Conditional Fix

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,10 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'core-js';
-import 'regenerator-runtime/runtime';
+// import 'regenerator-runtime/runtime';
 // eslint-disable-next-line import/no-unresolved
-// import App from './components/App.jsx';
 import App from '#components/App';
-console.time('test');
 
 ReactDOM.render(<App />, document.getElementById('app'));
