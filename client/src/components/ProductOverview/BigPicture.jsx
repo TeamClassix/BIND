87475b7c6c6fcc514588e27667177cc73bdf86bd
Jesse <jessemchung1@gmail.com>
2021-07-29 11:46:17 -0700

    zoom technically works2

--- client/src/components/ProductOverview/BigPicture.jsx
+++ client/src/components/ProductOverview/BigPicture.jsx
@@ -105,9 +105,6 @@ width:100%;
       </div>
       <ZoomImage src={props.imgURL} />
 
-
-        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"/>
-
     </>
   );
 };
