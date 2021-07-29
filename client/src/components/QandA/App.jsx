51a2790215fea6ac038992c188d800aba6aa7d27
Nhu Mai <nhumaix3@gmail.com>
2021-07-29 01:26:15 -0700

    rendered questions to page, worked on question modal, worked on answer modal, rendered helpfulness

--- client/src/components/QandA/App.jsx
+++ client/src/components/QandA/App.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 import QandAContainer from './QandAContainer';
-import MoreQuestions from './MoreQuestions';
-import AddQuestion from './AddQuestion';
+import QuestionModal from './QuestionModal';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const App = ({ productId }) => {
+  const [questions, setQuestions] = useState([]);
+  const [questionShow, setQuestionShow] = useState(false);
 
-    };
-  }
+  const getQuestions = () => (
+    axios.get('/api/qa/questions', {
+      params: {
+        product_id: productId,
+      },
+    })
+      .then((response) => {
+        setQuestions(response.data.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  );
 
-  render() {
-    return (
-      <div>
-        <h1>Questions and Answers</h1>
-        <SearchBar />
-        <QandAContainer />
-        <MoreQuestions />
-        <AddQuestion />
-      </div>
+  useEffect(getQuestions, []);
 
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Questions and Answers</h1>
+      <SearchBar />
+      <QandAContainer questions={questions} />
+      <button type="submit">More Questions</button>
+      <button type="submit" onClick={() => setQuestionShow(true)}>Add A Question</button>
+      <QuestionModal onClose={() => setQuestionShow(false)} show={questionShow} />
+    </div>
+  );
+};
 
 export default App;
-
-//contains search bar, q and a container, more questions, and add question
--- client/src/components/QandA/QuestionItem.jsx
+++ client/src/components/QandA/QuestionItem.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AnswerModal from './AnswerModal';
 
-const QuestionItem = () => (
-  <div>Question Item</div>
-);
+const QuestionItem = ({ question, helpful }) => {
+  const [answerShow, setAnswerShow] = useState(false);
+  const [helpfulness, setHelpfulness] = useState(helpful);
+
+  const handleAddAnswer = () => (setAnswerShow(true));
+  const handleHelpfulness = () => setHelpfulness(helpfulness++);
+
+  return (
+    <div>
+      <div className="question">
+        <h4>{`Q: ${question}`}</h4>
+      </div>
+      <div>
+        Helpful?
+      </div>
+      <div className="yes" onClick={handleHelpfulness}>
+        {`Yes(${helpfulness}) | `}
+      </div>
+      <div onClick={handleAddAnswer}>Add Answer</div>
+      <AnswerModal onClose={() => setAnswerShow(false)} show={answerShow} />
+    </div>
+  );
+};
 
 export default QuestionItem;
